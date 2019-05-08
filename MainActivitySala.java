31b28645bba12fe1adfdd0d2c822c2ef3f788ce8
juarodbau <48862063+juarodbau@users.noreply.github.com>
2019-05-08 22:40:43 +0200

    Update MainActivitySala.java

--- MainActivitySala.java
+++ MainActivitySala.java
@@ -1,6 +1,9 @@
 package com.dam.appcliente2;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +13,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -24,35 +30,126 @@ public class MainActivitySala extends AppCompatActivity {
     ImageView mazo4;
 
     private Button atras2;
+    private ImageView totem;
+    private Socket[] sss = new Socket[5];
+    private int i = 0;
+    /**
+     * Puerto
+     * */
+    private static final int SERVERPORT = 54325;
+    /**
+     * HOST
+     * */
+    private static final String ADDRESS = "192.168.0.27";
+
+    private Context context = this;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sala);
-        Log.d("LOG", "en sala so");
-
-        Cliente myClient = new Cliente("192.168.0.27",54322);
-        myClient.execute();
-        //mazo1 = (ImageView) findViewById(R.id.mazo1);
-        //mazo2 = (ImageView) findViewById(R.id.mazo2);
-        //mazo3 = (ImageView) findViewById(R.id.mazo3);
-        //mazo4 = (ImageView) findViewById(R.id.mazo4);
 
         atras2 = (Button) findViewById(R.id.atrasButton2);
+        totem = (ImageView) findViewById(R.id.imageView17);
+
+        // hacer click en el boton de salir
+
         atras2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                try {
-                    socket.close();
-                }catch (IOException e){
 
-                    }
+                    MyATaskCliente myATaskYW = new MyATaskCliente();
+                    myATaskYW.execute("salir");
+
+
                 Intent intent = new Intent (v.getContext(), MainActivity.class);
                 startActivityForResult(intent, 2);
+            }
+        });
+
+    // Hacer click en el totem
+        totem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                MyATaskCliente myATaskYW = new MyATaskCliente();
+                myATaskYW.execute("tocado");
+
             }
         });
 
         Log.d("LOG", "en sala");
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        MyATaskCliente myATaskYW = new MyATaskCliente();
+        myATaskYW.execute("hola");
+
+    }
+    /**
+     * Clase para interactuar con el servidor
+     * */
+    class MyATaskCliente extends AsyncTask<String,Void,String> {
+
+        /**
+         * Ventana que bloqueara la pantalla del movil hasta recibir respuesta del servidor
+         */
+        ProgressDialog progressDialog;
+
+        /**
+         * Se conecta al servidor y trata resultado
+         */
+        @Override
+        protected String doInBackground(String... values) {
+
+            try {
+                Log.i("PRU", values[0]);
+                //Se conecta al servidor
+                InetAddress serverAddr = InetAddress.getByName(ADDRESS);
+                Socket socket = new Socket(serverAddr, SERVERPORT);
+                if ( i < 1) {
+                    sss[0] = socket; // Creamos un array de sockets para guardar el socket una vez el jugador entre en la sala
+                }else {
+                    sss[1] = socket;
+                }
+                    i++;
+                //envia peticion de cliente
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                String msg = values[0];
+                out.writeUTF(msg);
+                out.flush();
+                //cierra conexion
+
+              //  Log.i("LOGASO", request);
+
+                if (msg.equals("salir")) {
+                    DataOutputStream out2 = new DataOutputStream(sss[0].getOutputStream());
+                    out2.writeUTF(msg);
+                    out2.flush();
+                    socket.close();
+                }
+                if (msg.equals("tocado")) {
+                    DataOutputStream out2 = new DataOutputStream(sss[0].getOutputStream());
+                    out2.writeUTF(msg);
+                    out2.flush();
+
+                }
+            } catch (UnknownHostException ex) {
+                Log.e("E/TCP Client", "" + ex.getMessage());
+            } catch (IOException ex) {
+                Log.e("E/TCP Client", "" + ex.getMessage());
+            }
+            return "hola";
+        }
+
+        /**
+         * Oculta ventana emergente y muestra resultado en pantalla
+         */
+
+    }
 }
